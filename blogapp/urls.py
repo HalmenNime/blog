a6b476b0697418e7afde644e0019e25f213714bd
HalmenNime <leniv905@gmail.com>
2023-09-16 13:27:59 +0300

    Add index page for page

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("blogapp.urls"))
+    path("", view=views.index, name="home")
 ]
